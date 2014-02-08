91d05d2aa320456355c5bdf2a79b5b0a1ae630af
Neil Edelman <neil dot edelman each mail dot mcgill dot ca>
2014-02-08 03:20:31 -0500

    more try-catch!

--- www/session.php
+++ www/session.php
@@ -1,21 +1,34 @@
 <?php
 
-	/* database things */
-	//const COOKIE_TIME = 43200; /* 60s/m * 60m/h * 12h (seconds) */
-	define("COOKIE_TIME", 43200); /* yum yum yum */
+	/* 60s/m * 60m/h * 12h (seconds); yum yum yum */
+	define("COOKIE_TIME", 43200);
 
-	/** create new exception */
+	/** create new exception; this is sytactic sugar */
 	function throw_exception($message = null, $code = null) {
 		throw new Exception($message, $code);
 	}
 
-	/** called first */
+	/** called first! */
 	function persistent_session_start() {
 		/* probably should do this: set_error_handler("error_handler");*/
 		session_set_cookie_params(COOKIE_TIME/*, "/", "payom.ca" <- final */);
 		session_start();
 	}
-	
+
+	/** you will have to $db->close() */
+	function link_database() {
+
+		/* :3306? not working */
+		$db = new mysqli("127.0.0.1", "payomca_rms", "mushroom", "payomca_rms");		
+		if($db->connect_errno) {
+			die("link_database connect failed: (".$db->connect_errno.") ".$db->connect_error);
+		}
+		/* sane TZ! don't have to worry about DST */
+		$db->query("SET time_zone='+0:00'");
+
+		return $db;
+	}
+
 	/** returns the logged in user or null; fixme: timeout */
 	function get_logged_in_user($db) {
 
@@ -42,6 +55,7 @@
 					$logged = $username;
 				}
 			} catch(Exception $e) {
+				/* fixme: this is not the way to handle; set_error_handler? create a function error()? */
 				echo "is_logged_in ".$e->getMessage()." failed: (".$stmt->errno.") ".$stmt->error;
 			}
 			$stmt->close();
@@ -74,34 +88,28 @@
 	/** log in */
 	function login($db, $user, $pass) {
 
-		/* does the user exist in the database */
-		if(!($stmt = $db->prepare("SELECT password FROM "
-								  ."Users WHERE username = ? LIMIT 1"))) {
-			echo "login.1 prepare failed: (".$db->errno.") ".$db->error;
-			return false;
-		}
-		if(!$stmt->bind_param("s", $user)) {
-			echo "login.1 binding failed: (".$stmt->errno.") ".$stmt->error;
+		/* does the user exist in the database and the password match? */
+		$return = false;
+		try {
+			$stmt = $db->prepare("SELECT password FROM "
+								 ."Users WHERE username = ? "
+								 ."LIMIT 1") or throw_exception("prepare");
+			try {
+				$stmt->bind_param("s", $user) or throw_exception("binding");
+				$stmt->execute() or throw_exception("execute");
+				$stmt->bind_result($server_pass);
+				if($stmt->fetch() && password_verify($pass, $server_pass)) {
+					$return = true;
+				}
+			} catch(Exception $e) {
+				echo "login search ".$e->getMessage()." failed: (".$stmt->errno.") ".$stmt->error;
+			}
 			$stmt->close();
-			return false;
+		} catch(Exception $e) {
+			echo "login search ".$e->getMessage()." failed: (".$db->errno.") ".$db->error;
 		}
-		if(!$stmt->execute()) {
-			echo "login.1 execute failed: (".$stmt->errno.") ".$stmt->error;
-			$stmt->close();
-			return false;
-		}
-		$stmt->bind_result($server_pass);
-		if(!$stmt->fetch()) {
-			echo "login.1 fetching failed: (".$db->errno.") ".$db->error;
-			$stmt->close();
-			return false;
-		}
-		/* there is no record of it on the server; the user hasn't logged in */
-		if(!password_verify($pass, $server_pass)) {
-			$stmt->close();
-			return false;
-		}
-		$stmt->close();
+
+		if(!$return) return false;
 
 		/* store the session - local version */
 		$session                          = session_id();
@@ -110,63 +118,69 @@
 		$activity = $_SESSION["activity"] = gmdate("Y-m-d H:i:s");
 
 		/* store on the server */
-		if(!($stmt = $db->prepare("INSERT INTO "
-								  ."SessionID(session_id, username, ip, activity)"
-								  ." VALUES (?, ?, ?, ?)"))) {
-			echo "login.2 prepare failed: (".$db->errno.") ".$db->error;
-			return false;
-		}
-		if(!$stmt->bind_param("ssss", $session, $user, $ip, $activity)) {
-			echo "login.2 binding failed: (".$stmt->errno.") ".$stmt->error;
+		$return = false;
+		try {
+			$stmt = $db->prepare("INSERT INTO "
+								 ."SessionID(session_id, username, ip, activity)"
+								 ." VALUES (?, ?, ?, ?)") or throw_exception("prepare");
+			try {
+				$stmt->bind_param("ssss", $session, $user, $ip, $activity) or throw_exception("binding");
+				$stmt->execute() or throw_exception("execute");
+				$return = true;
+			} catch(Exception $e) {
+				echo "login store ".$e->getMessage()." failed: (".$stmt->errno.") ".$stmt->error;
+			}
 			$stmt->close();
-			return false;
+		} catch(Exception $e) {
+			echo "login store ".$e->getMessage()." failed: (".$db->errno.") ".$db->error;
 		}
-		if(!$stmt->execute()) {
-			echo "login.2 execute failed: (".$stmt->errno.") ".$stmt->error;
-			$stmt->close();
-			return false;
-		}
-		$stmt->close();
 
-		return true;
+		return $return;
 	}
 
 	/** logs you out of your session */
 	function logoff($db) {
-		if(!($stmt = $db->prepare("DELETE FROM "
-								  ."SessionID WHERE session_id = ? LIMIT 1"))) {
-			echo "logoff prepare failed: (".$db->errno.") ".$db->error;
-			return false;
-		}
-		if(!$stmt->bind_param("s", session_id())) {
-			echo "logoff binding failed: (".$stmt->errno.") ".$stmt->error;
+		$return = false;
+		try {
+			$stmt = $db->prepare("DELETE FROM "
+								 ."SessionID WHERE session_id = ? "
+								 ."LIMIT 1") or throw_exception("prepare");
+			try {
+				$stmt->bind_param("s", session_id()) or throw_exception("binding");
+				$stmt->execute() or throw_exception("execute");
+				$return = true;
+			} catch(Exception $e) {
+				echo "logoff ".$e->getMessage()." failed: (".$stmt->errno.") ".$stmt->error;
+			}
 			$stmt->close();
-			return false;
+		} catch(Exception $e) {
+			echo "logoff ".$e->getMessage()." failed: (".$db->errno.") ".$db->error;
 		}
-		if(!$stmt->execute()) {
-			echo "logoff execute failed: (".$stmt->errno.") ".$stmt->error;
-			$stmt->close();
-			return false;
-		}
-		$stmt->close();
 
-		session_destroy();
+		if($return) session_destroy();
 
-		return true;
+		return $return;
 	}
 
-	/** you will have to $db->close() */
-	function link_database() {
-
-		/* :3306? not working */
-		$db = new mysqli("127.0.0.1", "payomca_rms", "mushroom", "payomca_rms");		
-		if($db->connect_errno) {
-			die("link_database connect failed: (".$db->connect_errno.") ".$db->connect_error);
+	/** new user? assumes valid input */
+	function new_user($db, $user, $pass, $first, $last) {
+		$return = false;
+		try {
+			$stmt = $db->prepare("INSERT INTO "
+								 ."Users(username, password, FirstName, LastName) "
+								 ."VALUES (?, ?, ?, ?)") or throw_exception("prepare");
+			try {
+				$stmt->bind_param("ssss", $user, $pass, $first, $last) or throw_exception("binding");
+				$stmt->execute() or throw_exception("execute");
+				$return = true;
+			} catch(Exception $e) {
+				echo "new_user ".$e->getMessage()." failed: (".$stmt->errno.") ".$stmt->error;
+			}
+			$stmt->close();
+		} catch(Exception $e) {
+			echo "new_user ".$e->getMessage()." failed: (".$db->errno.") ".$db->error;
 		}
-		/* sane TZ! don't have to worry about DST */
-		$db->query("SET time_zone='+0:00'");
-
-		return $db;
+		return $return;
 	}
 
 	/** this is a alias of versions > 5.3 */
@@ -180,28 +194,6 @@
 	/** this is a alias of versions > 5.3 */
 	function password_verify($plain, $hash) {		
 		return crypt($plain, $hash) == $hash;
-	}
-
-	/** new user? assumes valid input */
-	function new_user($db, $user, $pass, $first, $last) {
-		if(!($stmt = $db->prepare("INSERT INTO "
-								  ."Users(username, password, FirstName, LastName)"
-								  ." VALUES (?, ?, ?, ?)"))) {
-			echo "new_user prepare failed: (".$db->errno.") ".$db->error;
-			return false;
-		}
-		if(!$stmt->bind_param("ssss", $user, $pass, $first, $last)) {
-			echo "new_user binding failed: (".$stmt->errno.") ".$stmt->error;
-			$stmt->close();
-			return false;
-		}
-		if(!$stmt->execute()) {
-			echo "new_user execute failed: (".$stmt->errno.") ".$stmt->error;
-			$stmt->close();
-			return false;
-		}
-		$stmt->close();
-		return true;
 	}
 
 ?>
