062491645e4ab073f21215e2981fe5d52724e89a
yiqing.xiao <xiaoyiqing90@gmail.com>
2014-02-20 21:46:10 -0500

    testing data passing between pages

---
+++ www/passing-var.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+		<meta name = "Author" content = "Team RMS">
+		<link rel = "shortcut icon" href = "favicon.ico" type = "image/x-icon">
+		<link rel = "stylesheet" type = "text/css" href = "style.css">
+        <title>Create Table</title>
+    </head>
+	    <body>
+        <form method="post" action="catching-var.php">
+		
+		1. <input type="text" name = "name1"/><br/>
+		2. <input type="text" name = "name2"/><br/>
+		3. <input type="text" name = "name3"/><br/>
+		4. <input type="text" name = "name4"/><br/>
+		
+		<input type="submit" name="submit"/>
+		
+		</form>
+		</body>
+</html>
