62514040ee07dafae515d865acd500f39fd2c310
yiqing.xiao <xiaoyiqing90@gmail.com>
2014-02-20 21:54:48 -0500

    setting up the initial php script of edittable.php

--- www/edittable.php
+++ www/edittable.php
@@ -9,12 +9,38 @@
 	$info = $s->user_info($user) or header_error("user info error");
 	is_admin($info) or header_error("not authorised");
 
-	/* if the things are set, get them into vars */
-	isset($_REQUEST["username"])  and $username = strip_tags(stripslashes($_REQUEST["username"]));
-	isset($_REQUEST["password"])  and $_REQUEST["password"] != "" and $password = $_REQUEST["password"];
-	isset($_REQUEST["firstname"]) and $first    = strip_tags(stripslashes($_REQUEST["firstname"]));
-	isset($_REQUEST["lastname"])  and $last     = strip_tags(stripslashes($_REQUEST["lastname"]));
-	isset($_REQUEST["email"])     and $email    = strip_tags(stripslashes($_REQUEST["email"]));
-	isset($_REQUEST["privilege"]) and $privilege= strip_tags(stripslashes($_REQUEST["privilege"]));
+	$tablenumber = $_POST['tablenumber'];
+	$maxsize = $_POST['maxsize'];
+	$currentsize = $_POST['currentsize'];
+	$status = $_POST['status'];
+	
+		/* if the things are set, get them into vars */
+	isset($_REQUEST["tablenumber"])	and $tablenumber 	= strip_tags(stripslashes($_REQUEST["tablenumber"]));
+	isset($_REQUEST["maxsize"]) 	and $maxsize    	= strip_tags(stripslashes($_REQUEST["maxsize"]));
+	isset($_REQUEST["currentsize"])	and $currentsize   	= strip_tags(stripslashes($_REQUEST["currentsize"]));
+	isset($_REQUEST["status"])     	and $status			= strip_tags(stripslashes($_REQUEST["status"]));
 
-?>
+?> 
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+		<meta name = "Author" content = "Team RMS">
+		<link rel = "shortcut icon" href = "favicon.ico" type = "image/x-icon">
+		<link rel = "stylesheet" type = "text/css" href = "style.css">
+        <title>Create Table</title>
+    </head>
+	    <body>
+        <form method="post" action="catching-var.php">
+		
+		1. <input type="text" name = "name1"/><br/>
+		2. <input type="text" name = "name2"/><br/>
+		3. <input type="text" name = "name3"/><br/>
+		4. <input type="text" name = "name4"/><br/>
+		
+		<input type="submit" name="submit"/>
+		
+		</form>
+		</body>
+</html>
