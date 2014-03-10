242e1b8cdefa874c832a554f262fbe2bd1369e69
JonathanBouchard <jonathan.bouchard2@mail.mcgill.ca>
2014-03-09 23:58:21 -0400

    Fixed conflicts with the merge

--- www/viewtables.php
+++ www/viewtables.php
@@ -58,24 +58,4 @@
     </body>
 </html>
 
-        <meta name = "Author" content = "Team RMS">
-        <link rel = "shortcut icon" href = "favicon.ico" type = "image/x-icon">
-        <link rel = "stylesheet" type = "text/css" href = "style.css">
-        <title>View Tables</title>
-    </head>
-    <body>
-        <h1>View Tables</h1>
-
-        <?php
-             echo $g->display_all_tables($all_tables);
-
-             $all_tables->close();
-        ?>
-
-        <p>
-            <br>Go back to the <a href = "mainmenu.php">main menu</a>.
-        </p>
-
-    </body>
-</html>
-
+     
