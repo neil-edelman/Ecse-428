b8a3d6b15773eae5c94cefbdd6a4dacbf146ca06
Payom Meshgin <mail@payom.ca>
2014-03-16 08:52:47 -0400

    Showing all revenues now

---
+++ www/revenues.php
@@ -0,0 +1,60 @@
+<?php
+
+class Revenues {
+
+    public function get_daily_revenues($db) {
+        // Generate table
+        $table = $table . "<table>";
+
+        $table = $table
+                . "<thead>"
+                . "<tr>"
+                . "<th bgcolor=\"silver\">Date</th>"
+                . "<th bgcolor=\"silver\">Revenue</th>"
+                . "</tr>"
+                . "</thead>";
+
+        $table = $table . "<tbody>";
+
+        // Get earliest date
+        $sqlQuery = "SELECT Min(date) FROM Bills";
+        $result = mysqli_query($db, $sqlQuery);
+        $result_date = mysqli_fetch_array($result);
+        $earliest_date = date_create($result_date[0]);
+        echo date_format($earliest_date, "Y-m-d") . '<br>';
+
+        // Assume last date is current date
+        $latest_date = date_create(date("Y-m-d"));
+        echo date_format($latest_date, "Y-m-d") . '<br>';
+
+        // iterate through all dates
+        $curr_date = $latest_date;
+        while ($curr_date >= $earliest_date) {
+            $curr_date_str = date_format($curr_date, "Y-m-d") . '<br>';
+            $table = $table . "<td>$curr_date_str</td>";
+
+            $sqlQuery = "SELECT SUM(revenue) FROM Bills WHERE date='" . $curr_date_str . "'";
+            $result = mysqli_query($db, $sqlQuery);
+            $daily_revenue = mysqli_fetch_array($result, MYSQLI_BOTH);
+
+            if ($result == NULL) {
+                $table = $table . "<td>0.00</td>";
+            } else {
+                $table = $table . "<td>$daily_revenue[0]</td>";
+            }
+
+            mysqli_free_result($result, MYSQLI_BOTH);
+
+            $table = $table . "</tr>";
+
+            date_sub($curr_date, new DateInterval('P1D'));
+        }
+
+        mysqli_close($db);
+
+        $table = $table . "</tbody></table>";
+
+        echo $table;
+    }
+
+}
--- www/viewrevenues.php
+++ www/viewrevenues.php
@@ -1,59 +1,38 @@
 <?php
-    include "session.php";
+include "session.php";
+include "revenues.php";
 
-    $s = new Session();
+$s = new Session();
+$r = new Revenues();
 
-    $db = $s->link_database() or header_error("database error");
-    $user = $s->get_user() or header_error("user timeout error");
-
+$db = $s->link_database() or header_error("database error");
+$user = $s->get_user() or header_error("user timeout error");
+$info = $s->user_info($user) or header_error("user info error");
+is_admin($info) or header_error("not authorised");
 ?>
 
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
-		<meta name = "Author" content = "Team RMS">
-		<link rel = "shortcut icon" href = "favicon.ico" type = "image/x-icon">
-                <link rel = "stylesheet" type = "text/css" href = "style.css">
-		<title>View Tables</title>
+        <meta name = "Author" content = "Team RMS">
+        <link rel = "shortcut icon" href = "favicon.ico" type = "image/x-icon">
+        <link rel = "stylesheet" type = "text/css" href = "style.css">
+        <title>View Revenues</title>
     </head>
     <body>
-        <h1>View Tables</h1>        
-           
-<!--        <form name="select" method="post" action="viewStats.php">
-            Sort the table by:
-            <select name="select_ordering" onchange="this.form.submit()">
-                <option value="empty">CHOOSE OPTION</option>
-                <option value="tablenumber ASC">Table Number (INCR)</option>
-                <option value="tablenumber DESC">Table Number (DECR)</option>
-                <option value="maxsize ASC">Max Size (INCR)</option>
-                <option value="maxsize DESC">Max Size (DECR)</option>
-                <option value="currentsize ASC">Current Size (INCR)</option>
-                <option value="currentsize DESC">Current Size (DECR)</option>
-                <option value="status ASC">Status (INCR)</option>
-                <option value="status DESC">Status (DECR)</option>
-            </select>
-        </form>          -->
-        
-        <?php            
-//            if ($ordering = filter_input(INPUT_POST, 'select_ordering', FILTER_DEFAULT)) {                
-//                $all_tables = $g->get_all_tables($db, $ordering); 
-//            } else {                
-//                $all_tables = $g->get_all_tables($db, "default");
-//           }
-            
-            echo $g->display_all_tables($all_tables, $db);      
-                
-            $all_tables->close();
+        <h1>View Revenues</h1>        
+
+        <?php
+        echo $r->get_daily_revenues($db);
         ?>                    
-        
+
         <p>
-			<br>
-<!--            Go to <a href = "createtable.php">Create Tables</a><br>-->
-			Go back to the <a href = "mainmenu.php">main menu</a>.			
+            <br>
+            Go back to the <a href = "mainmenu.php">main menu</a>.			
         </p>
-        
+
     </body>
 </html>
 
-     
+
