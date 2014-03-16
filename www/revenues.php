187be32394c124f1214608d66bef6fa44ad4d3d2
Payom Meshgin <mail@payom.ca>
2014-03-16 09:53:03 -0400

    Removed debug messages, also days without revenues display 0.00 as value

--- www/revenues.php
+++ www/revenues.php
@@ -21,11 +21,11 @@ class Revenues {
         $result = mysqli_query($db, $sqlQuery);
         $result_date = mysqli_fetch_array($result);
         $earliest_date = date_create($result_date[0]);
-        echo date_format($earliest_date, "Y-m-d") . '<br>';
+        date_format($earliest_date, "Y-m-d") . '<br>';
 
         // Assume last date is current date
         $latest_date = date_create(date("Y-m-d"));
-        echo date_format($latest_date, "Y-m-d") . '<br>';
+        date_format($latest_date, "Y-m-d") . '<br>';
 
         // iterate through all dates
         $curr_date = $latest_date;
@@ -36,14 +36,14 @@ class Revenues {
             $sqlQuery = "SELECT SUM(revenue) FROM Bills WHERE date='" . $curr_date_str . "'";
             $result = mysqli_query($db, $sqlQuery);
             $daily_revenue = mysqli_fetch_array($result, MYSQLI_BOTH);
-
-            if ($result == NULL) {
+            
+            if ($daily_revenue[0] === NULL) {
                 $table = $table . "<td>0.00</td>";
             } else {
                 $table = $table . "<td>$daily_revenue[0]</td>";
             }
 
-            mysqli_free_result($result, MYSQLI_BOTH);
+            mysqli_free_result($result);
 
             $table = $table . "</tr>";
 
