6e921bbbad8fa7b88df16f3655ab83d42cbaf9d2
yiqing.xiao <xiaoyiqing90@gmail.com>
2014-03-21 15:33:32 -0400

    edituser improved

--- www/edituser.php
+++ www/edituser.php
@@ -19,10 +19,10 @@
 		$_SESSION["oriemail"] = $pieces[3];
 		$_SESSION["oriprivilege"] = $pieces[4];
 		$inusername = $pieces[0];
-		$first = $pieces[1];
-		$last = $pieces[2];
-		$email = $pieces[3];
-		$privilege = $pieces[4];
+		$infirst = $pieces[1];
+		$inlast = $pieces[2];
+		$inemail = $pieces[3];
+		$inprivilege = $pieces[4];
 	}
 	
 	if(isset($_SESSION['submitted'])){
@@ -30,14 +30,85 @@
 		unset($_SESSION['submitted']);
 	}
 	
-	/* if the things are set, get them into vars */
-	isset($_REQUEST["inusername"])	and $inusername 	= strip_tags(stripslashes($_REQUEST["inusername"]));
-	isset($_REQUEST["password"]) 	and $password    	= strip_tags(stripslashes($_REQUEST["password"]));
-	isset($_REQUEST["first"])   	and $first			= strip_tags(stripslashes($_REQUEST["first"]));
-	isset($_REQUEST["last"])		and $last   		= strip_tags(stripslashes($_REQUEST["last"]));
-	isset($_REQUEST["email"])   	and $email			= strip_tags(stripslashes($_REQUEST["email"]));
-	isset($_REQUEST["privilege"])   and $privilege		= strip_tags(stripslashes($_REQUEST["privilege"]));
-
+	
+	// if the things are set, get them into vars 
+	isset($_REQUEST["inusername"])	and $inusername 	= strip_tags(stripslashes($_REQUEST["inusername"]))
+		and $username = $inusername;
+	isset($_REQUEST["inpassword"]) 	and $inpassword    	= strip_tags(stripslashes($_REQUEST["inpassword"]))
+		and $password = $inpassword;
+	isset($_REQUEST["infirst"])   	and $infirst			= strip_tags(stripslashes($_REQUEST["infirst"]))
+		and $first = $infirst;
+	isset($_REQUEST["inlast"])		and $inlast   		= strip_tags(stripslashes($_REQUEST["inlast"]))
+		and $last = $inlast;
+	isset($_REQUEST["inemail"])   	and $inemail			= strip_tags(stripslashes($_REQUEST["inemail"]))
+		and $email = $inemail;
+	isset($_REQUEST["inprivilege"])   and $inprivilege		= strip_tags(stripslashes($_REQUEST["inprivilege"]))
+		and $privilege = $inprivilege;
+	
+	$is_ready = false;
+	if(   isset($username)
+	   || isset($password)
+	   || isset($first)
+	   || isset($last)
+	   || isset($email)
+	   || isset($privilege)) {
+		$is_ready = true;
+		if(   !isset($username)
+		   || !isset($password)
+		   || !isset($first)
+		   || !isset($last)
+		   || !isset($email)
+		   || !isset($privilege)
+		   || empty($username)
+		   || empty($password)
+		   || empty($first)
+		   || empty($last)
+		   || empty($email)
+		   || empty($privilege)) {
+			$is_ready = false;
+			echo "You did not enter all the required information.<br/>\n";
+		}
+		if(strlen($username) > Session::USERNAME_MAX) {
+			$is_ready = false;
+			echo "Username is maximum ".Session::USERNAME_MAX." characters.<br/>\n";
+		}
+		if(strlen($password) > Session::PASSWORD_MAX) {
+			$is_ready = false;
+			echo "Password is too long.<br/>\n";
+		}
+		if(strlen($first) > Session::FIRST_MAX) {
+			$is_ready = false;
+			echo "First name is maximum ".Session::FIRST_MAX." characters.<br/>\n";
+		}
+		if(strlen($last) > Session::LAST_MAX) {
+			$is_ready = false;
+			echo "Last name is maximum ".Session::LAST_MAX." characters.<br/>\n";
+		}
+		if(strlen($email) > Session::EMAIL_MAX) {
+			$is_ready = false;
+			echo "E-mail is maximum ".Session::EMAIL_MAX." characters.<br/>\n";
+		}else if(!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/",$email)){
+			$is_ready = false;
+			echo "Not a valid email format.<br/>\n";
+		}
+	}
+	if($is_ready) {
+		if($s->edit_user($_SESSION['oriusername'], $username, $password, $first, $last, $email, $privilege)) {
+			
+			//Clear out temporary Session variables
+			unset($_SESSION['oriusername']);
+			unset($_SESSION["orifirst"]);
+			unset($_SESSION["orilast"]);
+			unset($_SESSION["oriemail"]);
+			unset($_SESSION["oriprivilege"]);
+			$_SESSION['submitted'] = true;
+			
+			Header('Location: '.$_SERVER['PHP_SELF']);
+		} else {
+			echo "Account not edited: ".$s->status()."<br/>\n";
+		}
+	}
+	
 ?>
 <!DOCTYPE html>
 <html>
@@ -57,7 +128,7 @@
 			echo "Original User username: 	 &quot;".$_SESSION["oriusername"]."&quot;<br/>\n";
 			echo "Original User firstname: 	 &quot;".$_SESSION["orifirst"]."&quot;<br/>\n";
 			echo "Original User lastname: 	 &quot;".$_SESSION["orilast"]."&quot;<br/>\n";
-			echo "Original User email: 		 &quot;".$_SESSION["oriemail"]."&quot;<br/>\n";
+			echo "Original User inemail: 		 &quot;".$_SESSION["oriemail"]."&quot;<br/>\n";
 			echo "Original User privilege: 	 &quot;".$_SESSION["oriprivilege"]."&quot;<br/>\n";
 			?>
 			<br/>
@@ -69,31 +140,31 @@
 			maxlength = "<?php echo Session::USERNAME_MAX;?>"/><br/>
 
             <label>New Password:</label>
-			<input type="password" name="password"
-			value = "<?php if(isset($password)) echo $password;?>" 
+			<input type="inpassword" name="inpassword"
+			value = "<?php if(isset($inpassword)) echo $inpassword;?>" 
 			maxlength = "<?php echo Session::PASSWORD_MAX;?>"/><br/>
 
             <label>New First Name:</label>
-			<input type="text" name="first"
-			value = "<?php if(isset($first)) echo $first;?>"  
+			<input type="text" name="infirst"
+			value = "<?php if(isset($infirst)) echo $infirst;?>"  
 			maxlength = "<?php echo Session::FIRST_MAX;?>"/><br/>
 
             <label>New Last Name:</label>
-			<input type="text" name="last"
-			value = "<?php if(isset($last)) echo $last;?>"
+			<input type="text" name="inlast"
+			value = "<?php if(isset($inlast)) echo $inlast;?>"
 			maxlength = "<?php echo Session::LAST_MAX;?>"/><br/>
 			
             <label>Email:</label>
-			<input type="text" name="email"
-			value = "<?php if(isset($email)) echo $email;?>"
+			<input type="text" name="inemail"
+			value = "<?php if(isset($inemail)) echo $inemail;?>"
 			maxlength = "<?php echo Session::EMAIL_MAX;?>"/><br/>
 			
             <label>Privilege:</label>
-            <select name="privilege">
-                <option <?php if(isset($privilege)) echo $privilege=="wait"?"selected ":"";?>value="wait">Wait Staff</option>
-                <option <?php if(isset($privilege)) echo $privilege=="cook"?"selected ":"";?>value="cook">Cook Staff</option>
-                <option <?php if(isset($privilege)) echo $privilege=="manager"?"selected ":"";?>value="manager">Manager</option>
-                <option <?php if(isset($privilege)) echo $privilege=="admin"?"selected ":"";?>value="admin">System Admin</option>
+            <select name="inprivilege">
+                <option <?php if(isset($inprivilege)) echo $inprivilege=="wait"?"selected ":"";?>value="wait">Wait Staff</option>
+                <option <?php if(isset($inprivilege)) echo $inprivilege=="cook"?"selected ":"";?>value="cook">Cook Staff</option>
+                <option <?php if(isset($inprivilege)) echo $inprivilege=="manager"?"selected ":"";?>value="manager">Manager</option>
+                <option <?php if(isset($inprivilege)) echo $inprivilege=="admin"?"selected ":"";?>value="admin">System Admin</option>
             </select><br/>
 			
             <br/>
@@ -108,74 +179,6 @@
 			</p>
 			</div>
         </form>		
-		
-		<?php
-			$is_ready = false;
-			if(   isset($inusername)
-			   || isset($password)
-			   || isset($first)
-			   || isset($last)
-			   || isset($email)
-			   || isset($privilege)) {
-				$is_ready = true;
-				if(   !isset($inusername)
-				   || !isset($password)
-				   || !isset($first)
-				   || !isset($last)
-				   || !isset($email)
-				   || !isset($privilege)
-				   || empty($inusername)
-				   || empty($password)
-				   || empty($first)
-				   || empty($last)
-				   || empty($email)
-				   || empty($privilege)) {
-					$is_ready = false;
-					echo "You did not enter all the required information.<br/>\n";
-				}
-				if(strlen($inusername) > Session::USERNAME_MAX) {
-					$is_ready = false;
-					echo "Username is maximum ".Session::USERNAME_MAX." characters.<br/>\n";
-				}
-				if(strlen($password) > Session::PASSWORD_MAX) {
-					$is_ready = false;
-					echo "Password is too long.<br/>\n";
-				}
-				if(strlen($first) > Session::FIRST_MAX) {
-					$is_ready = false;
-					echo "First name is maximum ".Session::FIRST_MAX." characters.<br/>\n";
-				}
-				if(strlen($last) > Session::LAST_MAX) {
-					$is_ready = false;
-					echo "Last name is maximum ".Session::LAST_MAX." characters.<br/>\n";
-				}
-				if(strlen($email) > Session::EMAIL_MAX) {
-					$is_ready = false;
-					echo "E-mail is maximum ".Session::EMAIL_MAX." characters.<br/>\n";
-				}else if(!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/",$email)){
-					$is_ready = false;
-					echo "Not a valid email format.<br/>\n";
-				}
-			}
-			if($is_ready) {
-				if($s->edit_user($_SESSION['oriusername'], $inusername, $password, $first, $last, $email, $privilege)) {
-					
-					/* Clear out temporary Session variables*/
-					unset($_SESSION['oriusername']);
-					unset($_SESSION["orifirst"]);
-					unset($_SESSION["orilast"]);
-					unset($_SESSION["oriemail"]);
-					unset($_SESSION["oriprivilege"]);
-					$_SESSION['submitted'] = true;
-					
-					Header('Location: '.$_SERVER['PHP_SELF']);
-				} else {
-					echo "Account not edited: ".$s->status()."<br/>\n";
-				}
-			}
-        ?>
-		
-		
 		
 		</body>
 	
