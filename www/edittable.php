7daae9ebda6aebcd67f850309c15feef7a6a050b
yiqing.xiao <xiaoyiqing90@gmail.com>
2014-03-21 14:56:20 -0400

    Edittable improvement

--- www/edittable.php
+++ www/edittable.php
@@ -15,21 +15,76 @@
 		$_SESSION["oritable"] = $pieces[0];
 		$_SESSION["orimaxsize"] = $pieces[1];
 		$_SESSION["oricurrentsize"] = $pieces[2];
-		$tablenumber = $pieces[0];
-		$maxsize = $pieces[1];
-		//$currentsize = $pieces[2];
+		$intablenumber = $pieces[0];
+		$inmaxsize = $pieces[1];
+		$incurrentsize = $pieces[2];
+		$infirstrun = true;
+	}else{
+		$firstrun = false;
 	}
 	
 	if(isset($_SESSION['submitted'])){
 		$submitted = $_SESSION['submitted'];
 		unset($_SESSION['submitted']);
-
 	}
 
 			/* if the things are set, get them into vars */
-	isset($_REQUEST["tablenumber"])	and $tablenumber 	= strip_tags(stripslashes($_REQUEST["tablenumber"]));
-	isset($_REQUEST["maxsize"]) 	and $maxsize    	= strip_tags(stripslashes($_REQUEST["maxsize"]));
-	isset($_REQUEST["currentsize"])	and $currentsize   	= strip_tags(stripslashes($_REQUEST["currentsize"]));	
+	isset($_REQUEST["intablenumber"])	and $intablenumber 	= strip_tags(stripslashes($_REQUEST["intablenumber"])) and $tablenumber = $intablenumber;
+	isset($_REQUEST["inmaxsize"]) 		and $inmaxsize    	= strip_tags(stripslashes($_REQUEST["inmaxsize"])) and $maxsize = $inmaxsize;
+	isset($_REQUEST["incurrentsize"])	and $incurrentsize  = strip_tags(stripslashes($_REQUEST["incurrentsize"])) and $currentsize = $incurrentsize;
+	isset($_REQUEST["infirstrun"])		and $infirstrun   	= strip_tags(stripslashes($_REQUEST["infirstrun"])) and $firstrun = $infirstrun;
+
+	$is_ready = false;
+	if(   isset($tablenumber)
+	   || isset($maxsize)
+	   || isset($currentsize)){
+		$is_ready = true;
+		if(   !isset($tablenumber)
+		   || !isset($maxsize)
+		   || !isset($currentsize)
+		   || empty($tablenumber)
+		   || empty($maxsize)
+		   || (empty($currentsize) && $currentsize != 0)) {
+			$is_ready = false;
+			echo "You did not enter all the required information.<br/>\n";
+		}
+		
+		if(strlen($tablenumber) > Session::INTEGER_MAX) {
+			$is_ready = false;
+			echo "Username is maximum ".Session::INTEGER_MAX." characters.<br/>\n";
+		}
+		if(strlen($maxsize) > Session::INTEGER_MAX) {
+			$is_ready = false;
+			echo "Password is too long.<br/>\n";
+		}
+		if(strlen($currentsize) > Session::INTEGER_MAX) {
+			$is_ready = false;
+			echo "First name is maximum ".Session::INTEGER_MAX." characters.<br/>\n";
+		}
+		if($currentsize > $maxsize){
+			$is_ready = false;
+			echo "current size is larger than the maximum size.";
+		}else if($currentsize > 0){
+			$status = 'occupied';
+		}else{
+			$status = 'vacant';
+		}
+		
+	}
+	if($is_ready) {
+		if($s->edit_table($_SESSION["oritable"], $tablenumber, $maxsize, $currentsize, $status)){
+			
+			/* Clear out temporary Session variables*/
+			unset($_SESSION['oritable']);
+			unset($_SESSION['orimaxsize']);
+			unset($_SESSION['oricurrentsize']);
+			$_SESSION['submitted'] = true;
+			
+			Header('Location: '.$_SERVER['PHP_SELF']);
+		} else {
+			echo "Table not edited: ".$s->status()."<br/>\n";
+		}
+	}
 ?>
 <!DOCTYPE html>
 <html>
@@ -41,109 +96,49 @@
         <title>Edit Table</title>
     </head>
 	    <body>
-        <form method="post">
+        <form method="post" action="edittable.php">
             <h1>Edit Table</h1>
 			<label>Currently: <br/></label>
 			<?php
-			echo "Original table number: 		 &quot;".$_SESSION["oritable"]."&quot;<br/>\n";
-			echo "Original table max size: 	 	 &quot;".$_SESSION["orimaxsize"]."&quot;<br/>\n";
-			echo "Original table current size: 	 &quot;".$_SESSION["oricurrentsize"]."&quot;<br/>\n";
+			if($firstrun){
+				echo "Original table number: 		 &quot;".$_SESSION["oritable"]."&quot;<br/>\n";
+				echo "Original table max size: 	 	 &quot;".$_SESSION["orimaxsize"]."&quot;<br/>\n";
+				echo "Original table current size: 	 &quot;".$_SESSION["oricurrentsize"]."&quot;<br/>\n";
+			}
 			?>
             <div>
 			<label>New Table ID:</label>
-<input type="text" name="tablenumber"
-value = "<?php if(isset($tablenumber)) echo $tablenumber;?>" 	
+<input type="text" name="intablenumber"
+value = "<?php if(isset($intablenumber)) echo $intablenumber;?>" 	
 maxlength = "<?php echo Session::INTEGER_MAX;?>"/><br/>
 
             <label>New Table Maximum Size:</label>
-<input type="text" name="maxsize"
-value = "<?php if(isset($maxsize)) echo $maxsize;?>" 
+<input type="text" name="inmaxsize"
+value = "<?php if(isset($inmaxsize)) echo $inmaxsize;?>" 
 maxlength = "<?php echo Session::INTEGER_MAX;?>"/><br/>
 
             <label>New Table Size:</label>
-<input type="text" name="currentsize"
-value = "<?php if(isset($currentsize)) echo $currentsize;?>"  
+<input type="text" name="incurrentsize"
+value = "<?php if(isset($incurrentsize)) echo $incurrentsize;?>"  
 maxlength = "<?php echo Session::INTEGER_MAX;?>"/><br/>
 
             <br/>
             <br/>
 			<input type = "submit" value = "Edit" <?php if (!isset($_SESSION["oritable"])){ echo "disabled";} ?>/>
-			<br/>
-			<input type = "reset" value = "Reset"/>
-			<br/>
 			<p><?php if(isset($submitted)) echo "Edit Complete.";?><br/>
 			<?php if (!isset($_SESSION["oritable"])){ echo "This page is presently stale.  Please return to mainmenu";} ?></p>
 			</div>
         </form>
 		
-		<p>Click the button to access an Input Button.</p>
-
-		<form action = "viewtables.php">
-			<input type="submit" value="Return to view table" onclick="myFunction()">
-		</form>
-		<script>
-		function myFunction()
-		{
 		<?php 
-			unset($_SESSION['oritable']);
-			unset($_SESSION['orimaxsize']);
-			unset($_SESSION['oricurrentsize']);
+			if($_SESSION['submitted']){
+				echo "submitted <br>";
+			}
 		?>
-		}
-		</script>
 		
-		<?php
-			$is_ready = false;
-			if(   isset($tablenumber)
-			   || isset($maxsize)
-			   || isset($currentsize)){
-				$is_ready = true;
-				if(   !isset($tablenumber)
-				   || !isset($maxsize)
-				   || !isset($currentsize)
-				   || empty($tablenumber)
-				   || empty($maxsize)
-				   || (empty($currentsize) && $currentsize != 0)) {
-					$is_ready = false;
-					echo "You did not enter all the required information.<br/>\n";
-				}
-				if(strlen($tablenumber) > Session::INTEGER_MAX) {
-					$is_ready = false;
-					echo "Username is maximum ".Session::INTEGER_MAX." characters.<br/>\n";
-				}
-				if(strlen($maxsize) > Session::INTEGER_MAX) {
-					$is_ready = false;
-					echo "Password is too long.<br/>\n";
-				}
-				if(strlen($currentsize) > Session::INTEGER_MAX) {
-					$is_ready = false;
-					echo "First name is maximum ".Session::INTEGER_MAX." characters.<br/>\n";
-				}
-				if($currentsize > $maxsize){
-					$is_ready = false;
-					echo "current size is larger than the maximum size.";
-				}else if($currentsize > 0){
-					$status = 'occupied';
-				}else{
-					$status = 'vacant';
-				}
-				
-			}
-			if($is_ready) {
-				if($s->edit_table($_SESSION["oritable"], $tablenumber, $maxsize, $currentsize, $status)){
-					
-					/* Clear out temporary Session variables*/
-					unset($_SESSION['oritable']);
-					unset($_SESSION['orimaxsize']);
-					unset($_SESSION['oricurrentsize']);
-					$_SESSION['submitted'] = true;
-					
-					Header('Location: '.$_SERVER['PHP_SELF']);
-				} else {
-					echo "Table not edited: ".$s->status()."<br/>\n";
-				}
-			}
-        ?>
+		<form action = "viewtables.php">
+			<input type="submit" value="Return to view table">
+		</form>
 		
 	</body>
 	
