d78d45ea64a4b17a62c7c51b6496daa47acc33fb
Neil Edelman <neil dot edelman each mail dot mcgill dot ca>
2014-03-23 15:51:10 -0400

    checkboxes, noprint

--- www/bill.php
+++ www/bill.php
@@ -11,11 +11,20 @@
 
 	/* if the things are set, get them into vars */
 	isset($_REQUEST["table"]) and $table  = strip_tags(stripslashes($_REQUEST["table"]));
-	isset($_REQUEST["order"]) and is_array($_REQUEST["order"]) and $order = $_REQUEST["order"];
+	isset($_REQUEST["check"]) and is_array($_REQUEST["check"]) and $check = $_REQUEST["check"];
 
-	function orderno($no) {
-		// common functionality
+	/** hack to see wheater is checked; O(n) :[ can do better
+	 @param check the value of the global var check (I have no idea why but it
+	 doesn't work without it,) ie, array with the check values
+	 @param value the order number that you want to see is checked or not
+	 @return checked or not
+	 @author Neil */
+	function is_checked($check, $value) {
+		if(!isset($check)) return false;
+		foreach($check as $c) if($c == $value) return true;
+		return false;
 	}
+
 ?>
 <!DOCTYPE html>
 
@@ -26,52 +35,10 @@
 		<link rel = "shortcut icon" href = "favicon.ico" type = "image/x-icon">
 		<link rel = "stylesheet" type = "text/css" href = "style.css">
 		<title>Bill</title>
-
-<script type = "text/javascript">
-<!--
-
-/* this keeps track of who we're editing */
-var editing = null;
-
-/** swiches to editing
- @param a string which identifies the element
- @author Neil */
-function edit(a) {
-	if(editing) hide(editing);
-	editing = a;
-
-	var textStatic = document.getElementById(a);
-	if(!textStatic) return;
-	textStatic.style.display = "none";
-
-	var textDynamic = document.getElementById(a.concat("edit"));
-	if(!textDynamic) return;
-	textDynamic.style.display = "block";
-}
-
-/** swiches back to looking
- @param a string which identifies the element
- @author Neil */
-function hide(a) {
-
-	var textStatic = document.getElementById(a);
-	if(!textStatic) return;
-	textStatic.style.display = "block";
-
-	var textDynamic = document.getElementById(a.concat("edit"));
-	if(!textDynamic) return;
-	textDynamic.style.display = "none";
-
-	editing = null;
-}
-
-// -->
-</script>
-
 	</head>
 	<body>
 
-	<h1>Bill</h1>
+	<h1 class = "noprint">Bill</h1>
 
 <?php
 	if(!isset($table)) {
@@ -94,10 +61,11 @@ function hide(a) {
 </form>
 </div></p>
 <?php
-	} else if(!isset($order)) {
+	} else {
 ?>
-<p>Table number <?php echo $table; ?>. Go <a href = "bill.php">back to table list</a>.</p>
-<p><div>
+<p class = "noprint">Table number <?php echo $table; ?>. Go <a href = "bill.php">back to table list</a>.</p>
+
+<div>
 <form>
 <input type = "hidden" name = "table" value = "<?php echo $table; ?>">
 <?php
@@ -125,9 +93,22 @@ function hide(a) {
 			$menu->bind_param("i", $item) or throw_exception("binding menu");
 
 			while($order->fetch()) {
+
+				// is it checked?
+				$ordercheck = is_checked($check, $orderid);
+
+				if($ordercheck) echo "<p>\n";
+				else            echo "<p class = \"noprint\">\n";
+
 				echo "<label>Order #".$orderid."</label>";
-				echo "<input type = \"checkbox\" name = \"order[]\" value = \"".$orderid."\"/><br/>\n";
-				echo "<label>Contains</label>";
+				if($ordercheck) echo "(checked) ";
+				else echo "(unchecked) ";
+
+				// provide the option to bill this on a new bill
+				if(!$ordercheck) echo "<input type = \"checkbox\" name = \"check[]\" value = \"".$orderid."\"/>\n";
+				echo "<br/>\n";
+				if(!$ordercheck) echo "<label>Contains</label>\n";
+
 				$contain->execute() or throw_exception("execute contain");
 				$contain->store_result();
 				$contain->bind_result($containid, $item, $quantity);
@@ -138,34 +119,54 @@ function hide(a) {
 					$menu->bind_result($menuname, $menucost, $menuitemid);
 					if($menu->fetch()) {
 						echo $quantity." ".$menuname." at ".$menucost."; ";
+						if($ordercheck) {
+							// update bill and remove from order
+						} else {
+						}
 					} else {
 						echo $quantity." unknown \"".$item."\"; ";
 					}
 					$menu->free_result(); /* fixme */
 				}
 
-				echo "<br/>\n";
+				echo "</p>\n";
 
 				$contain->free_result(); /* fixme */
 			}
 
 			$order->free_result(); /* fixme */
+			
+			echo "<p>check param: ";
+			if(isset($check)) {
+				foreach($check as $c) {
+					echo "contians ".$c."; ";
+				}
+			} else {
+				echo "is not there";
+			}
+			echo "</p>\n";
 
 		} catch(Exception $e) {
 			echo "<p>Error: ".htmlspecialchars($e->getMessage()).".</p>\n";
 		}
 ?>
-<br/>
-<label>&nbsp;</label><input type = "submit" value = "Go">
-</form>
-</div></p>
+<p class = "noprint">
+<label>&nbsp;</label><input type = "submit" value = "Go"><br/>
 <?php
-	} else {
-		echo "<p>".var_dump($order)."</p>\n";
+		if(isset($check)) {
+?>
+<label>&nbsp;</label><input type = "button" value = "Print" onclick = "window.print()"><br/>
+<?php
+		}
+?>
+</p>
+</form>
+</div>
+<?php
 	}
 ?>
 
-		<p>
+		<p class = "noprint">
 			Go back to the <a href = "mainmenu.php">main menu</a>.
 		</p>
 
